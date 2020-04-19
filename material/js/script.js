83a90f6caa8f249ad60adfc380dd1263319df0cc
Yana <bymogen@gmail.com>
2020-04-19 23:58:12 +0300

    Это наш первый комит

---
+++ material/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.log(leftBorderWidth);
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+
+/*"And"
+"ME!"*/
+
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symboll();
+// var boolean = true;
+// null;
+// undefined;
+// ver obj = {};
+
